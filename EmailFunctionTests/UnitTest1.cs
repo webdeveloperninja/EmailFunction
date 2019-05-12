1d063d15a158ff1bed64b143c9decff0d5690041
Your Name <rsmith5901@gmail.com>
2019-05-12 10:16:38 -0400

    fsdgdfgs

--- EmailFunctionTests/UnitTest1.cs
+++ EmailFunctionTests/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace EmailFunctionTests
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(1, 2);
+            Assert.AreEqual(1, 1);
         }
     }
 }
